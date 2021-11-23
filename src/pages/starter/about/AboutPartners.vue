5af2f9fd3490824e98593eeea24a0b2d9c3312ba
Nick <nicolas.ai.nomis@gmail.com>
2021-11-23 09:23:06 +0000

    [feat] 파트너스 carousel 추가

--- src/pages/starter/about/AboutPartners.vue
+++ src/pages/starter/about/AboutPartners.vue
@@ -1,34 +1,41 @@
 <template>
     <div class="container">
-        <h2 class="mt-5 mb-4 font-weight-bold text-center">Customers</h2>
-        <div class="row justify-content-center mb-5">
-            <div class="logo-container col-3 text-center" v-for="partner in partnerImages">
-                <img :src="partner" class="partner-logo">
-            </div>
-        </div>
+        <h2 class="mt-5 mb-4 font-weight-bold text-center">Partners</h2>
+            <el-carousel arrow="always" :autoplay="false" height="30vh">
+                <el-carousel-item v-for="start in (partnerImages.length / 4)">
+                    <div class="row px-5 align-content-center">
+                        <div class="logo-container col-6 col-md-3 text-center" v-for="partner in partnerImages.slice((start - 1) * 4, (start - 1) * 4 + 4)">
+                            <img :src="partner" class="partner-logo mb-3">
+                        </div>
+                    </div>
+                </el-carousel-item>
+            </el-carousel>
     </div>
 </template>
 
 <style scoped>
     .partner-logo {
         height: 100%;
-        object-fit: scale-down;
+        width: 100%;
+        object-fit: contain;
     }
 
     .logo-container {
-        height: 200px;
-        flex-grow : 0;
+        flex-grow: 0;
     }
 </style>
 
 <script>
     import { Card } from '@/components';
     import { Button } from '@/components';
+    import { Carousel, CarouselItem } from 'element-ui'
 
     export default {
         components: {
             Card,
             [Button.name]: Button,
+            [Carousel.name]: Carousel,
+            [CarouselItem.name]: CarouselItem
         },
         data() {
             return {
