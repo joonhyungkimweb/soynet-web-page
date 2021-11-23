23735a557a3c0a8e80e934911b8f5d0e14da9526
Nick <nicolas.ai.nomis@gmail.com>
2021-11-23 08:06:10 +0000

    [feat] 엘리먼트 ui css 추가

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ import App from "./App.vue";
 import router from "./starterRouter";
 //plugins
 import NowUIKit from './plugins/now-ui-kit';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.use(NowUIKit);
