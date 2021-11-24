d2d56f62f9bd863c79ed1eb4bc06701abcc7c4c4
Nick <nicolas.ai.nomis@gmail.com>
2021-11-24 10:59:01 +0000

    [feat] 블로그 카드 추가

---
+++ src/pages/starter/blogs/BlogsCards.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-md-10 ml-auto mr-auto">
+                <div v-for="({ category, title, thumbnail, description, buttonText }, index) in articles">
+                    <card type="blog" plain v-if="index % 2 === 0">
+                        <div slot="image" class="row">
+                            <div class="col-md-5 d-md-block d-none">
+                                <img class="img img-raised rounded" :src="thumbnail">
+                            </div>
+                            <div class="col-md-7">
+                                <h6 class="category text-info">
+                                    {{category}}
+                                </h6>
+                                <h3 class="card-title">
+                                    {{title}}
+                                </h3>
+                                <p class="card-description" v-html="Array.isArray(description) ? description.reduce((acc, cur) => `${acc} <li>${cur}</li>`,'<ul>') + '</ul>' : description">
+                                </p>
+                            </div>
+    
+                            <div class="col-md-5 d-md-none d-block">
+                                <img class="img img-raised rounded" :src="thumbnail">
+                            </div>
+                        </div>
+                    </card>
+    
+                    <card type="blog" plain v-else>
+                        <div class="row">
+                            <div class="col-md-7">
+                                <h6 class="category text-danger">
+                                    {{ category }}
+                                </h6>
+                                <h3 class="card-title">
+                                    {{ title }}
+                                </h3>
+                                <p class="card-description" v-html="Array.isArray(description) ? description.reduce((acc, cur) => `${acc} <li>${cur}</li>`,'<ul>') + '</ul>' : description">
+                                </p>
+                            </div>
+                            <div class="col-md-5">
+                                    <img class="img img-raised rounded" :src="thumbnail">
+                            </div>
+                        </div>
+                    </card>
+                </div>
+    
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+</style>
+
+<script>
+    import { Card } from '@/components';
+    import { Button } from '@/components';
+
+    export default {
+        components: {
+            Card,
+            [Button.name]: Button,
+        },
+        data() {
+            return {
+                articles: [{
+                        category: "General AI Topics",
+                        title: "AIpsum Lorem",
+                        description: "Phasellus eu gravida sapien. Phasellus vulputate ante nulla. Aliquam iaculis tortor non ex condimentum varius.",
+                        thumbnail: "img/examples/card-blog4.jpg",
+                    },
+                    {
+                        category: "Object Detection",
+                        title: "Artificial Intelligence",
+                        description: "Suspendisse quis ligula at nibh convallis blandit. Sed venenatis diam elit, vel ultrices velit lacinia nec. Vestibulum in elit vel ex lacinia aliquam.",
+                        thumbnail: "img/examples/card-blog6.jpg",
+                    }
+                ]
+            }
+        }
+    }
+</script>
--- src/pages/starter/blogs/BlogsPage.vue
+++ src/pages/starter/blogs/BlogsPage.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="blogs-page">
         <back-to-home/>
+        <h2 class="title text-center">Blog Posts</h2>
         <blog-slider/>
+        <divider class="my-5"/>
+        <blogs-cards/>
     </div>
 </template>
 
@@ -16,12 +19,14 @@
     import { Divider, BackToHome } from '@/components';
     
     import BlogSlider from './BlogSlider.vue';
+    import BlogsCards from './BlogsCards.vue';
 
     export default {
         components: {
             Divider,
             BackToHome,
-            BlogSlider
+            BlogSlider,
+            BlogsCards
         },
         data() {
             return {
