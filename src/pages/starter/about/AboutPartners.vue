83074f27744d6a821349db4a150fb28062d8792f
Nick <nicolas.ai.nomis@gmail.com>
2021-11-24 02:15:13 +0000

    [feat] Partners Contents swiper로 변경

--- src/pages/starter/about/AboutPartners.vue
+++ src/pages/starter/about/AboutPartners.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="container">
         <h2 class="mt-5 mb-4 font-weight-bold text-center">Partners</h2>
-            <el-carousel arrow="always" :autoplay="false" height="30vh">
-                <el-carousel-item v-for="start in (partnerImages.length / 4)">
-                    <div class="row px-5 align-content-center">
-                        <div class="logo-container col-6 col-md-3 text-center" v-for="partner in partnerImages.slice((start - 1) * 4, (start - 1) * 4 + 4)">
-                            <img :src="partner" class="partner-logo mb-3">
-                        </div>
-                    </div>
-                </el-carousel-item>
-            </el-carousel>
+        
+        <contents-swiper>
+        <template #slides>
+            <swiper-slide v-for="partner in partnerImages" class="logo-container px-4" :key="partner">
+                <img :src="partner" class="partner-logo"></img>
+            </swiper-slide>
+        </template>
+        </contents-swiper>
     </div>
 </template>
 
@@ -21,25 +20,24 @@
     }
 
     .logo-container {
-        flex-grow: 0;
+        width: 180;
+        height: 180px;
     }
 </style>
 
 <script>
-    import { Card } from '@/components';
-    import { Button } from '@/components';
-    import { Carousel, CarouselItem } from 'element-ui'
+    import { ContentsSwiper } from '@/components';
+
+    import { SwiperSlide } from "vue-awesome-swiper";
 
     export default {
         components: {
-            Card,
-            [Button.name]: Button,
-            [Carousel.name]: Carousel,
-            [CarouselItem.name]: CarouselItem
+            ContentsSwiper,
+            SwiperSlide
         },
         data() {
             return {
-                partnerImages: ['img/partners/kigam.png', 'img/partners/saltlux.png', 'img/partners/hundaiSteel.jpg', 'img/partners/posco.jpg']
+                partnerImages: ['img/partners/kigam.png', 'img/partners/saltlux.png', 'img/partners/hundaiSteel.jpg', 'img/partners/posco.jpg'],
             }
         }
     }
