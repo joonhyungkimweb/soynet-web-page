3cc396180ba83568acde1a56ede98fafa0697132
Nick <nicolas.ai.nomis@gmail.com>
2021-11-25 00:46:01 +0000

    {feat] About Header 레이아웃 변경

--- src/pages/starter/about/AboutHeader.vue
+++ src/pages/starter/about/AboutHeader.vue
@@ -1,43 +1,34 @@
 <template>
     <div class="header-container">
-            <div class="content-center">
-                <div class="container text-left">
-                    <div class="content-center">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="iframe-container">
-                                    <iframe height="400" src="https://www.youtube.com/embed/uexlz9vxjxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </div>
-                            </div>
-                            <div class="col-md-12 ml-auto mr-auto text-center">
-                                <h1 class="text-primary pt-4 mb-3 font-weight-bold">
-                                    SOYNET
-                                </h1>
-                                <h4 class="description">
-                                    SoyNet offers easier and more flexible ways to deliver AI-driven services.
-                                </h4>
-                                <n-button type="primary" size="lg" round>Who are we?</n-button>
-                            </div>
-                        </div>
+        <div class="content-center">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="iframe-container">
+                        <iframe height="400" src="https://www.youtube.com/embed/uexlz9vxjxo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
+                </div>
+                <div class="col-md-12 ml-auto mr-auto text-center">
+                    <h1 class="text-primary pt-4 mb-3 font-weight-bold">
+                        SOYNET
+                    </h1>
+                    <h4 class="description">
+                        SoyNet offers easier and more flexible ways to deliver AI-driven services.
+                    </h4>
+                        <n-button type="primary" size="lg" round href="#/what-is-soynet">Who are we?</n-button>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 <style scoped>
     .header-container {
-        min-height: 100vh;
+        min-height: calc(100vh - 650px);
         position: relative;
         overflow: hidden;
     }
 
     .content-center {
-        position: absolute;
-        top: 55%;
-        left: 50%;
-        z-index: 2;
-        -webkit-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
+        margin : 15vh auto 0;
         text-align: center;
         padding: 0 15px;
         width: 100%;
