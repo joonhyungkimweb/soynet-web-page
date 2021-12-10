c6e9d8735573fb24c938e5bc739bf1272e7415b7
Nick <nicolas.ai.nomis@gmail.com>
2021-12-10 08:06:48 +0000

    [feat] AboutCards 문구 변경

--- src/pages/about/AboutCards.vue
+++ src/pages/about/AboutCards.vue
@@ -20,13 +20,13 @@
                                 </p>
                                 <n-button type="primary" simple @click="buttonAction">{{ buttonText }}</n-button>
                             </div>
-    
+
                             <div class="col-md-5 d-md-none d-block">
                                 <img class="img img-raised rounded" :src="thumbnail">
                             </div>
                         </div>
                     </card>
-    
+
                     <card type="blog" plain v-else>
                         <div class="row">
                             <div class="col-md-7">
@@ -36,18 +36,18 @@
                                 <h3 class="card-title">
                                     {{ title }}
                                 </h3>
-                                <p class="card-description" v-html="Array.isArray(description) ? description.reduce((acc, cur) => `${acc} <li>${cur}</li>`,'<ul>') + '</ul>' : description">
+                                <p class="card-description" v-html="description">
                                 </p>
-    
+
                                 <n-button type="primary" simple @click="buttonAction">{{ buttonText }}</n-button>
                             </div>
                             <div class="col-md-5">
-                                    <img class="img img-raised rounded" :src="thumbnail">
+                                <img class="img img-raised rounded" :src="thumbnail">
                             </div>
                         </div>
                     </card>
                 </div>
-    
+
             </div>
         </div>
     </div>
@@ -56,9 +56,9 @@
     .about-card:not(:last-of-type) {
         margin-bottom: 80px;
     }
-    
-    .card-description{
-        color : #000000 !important;
+
+    .card-description {
+        color: #000000 !important;
     }
 </style>
 
@@ -76,26 +76,39 @@
                 articles: [{
                         category: "Deploy AI models",
                         title: "What we provide",
-                        description: "Highly optimized models with SOYNET. We reduce the AI models to 1/5th size and now you deploy it any platform (On-premises, Cloud, Edge)",
+                        description: "Highly optimized models with Soynet. We reduce the AI models to 1/5th size and now you deploy it any platform (On-premises, Cloud, Edge)",
                         thumbnail: "img/soynet/web-gd36244d23_1920.jpg",
-                        buttonText: "Learn more about Soynet",
-                        buttonAction : () => this.$router.push('/what-is-soynet')
+                        buttonText: "Learn more",
+                        buttonAction: () => this.$router.push('/what-is-soynet')
                     },
                     {
                         category: "Marketplace",
                         title: "Choose from our Model Market",
-                        description: "Model Market helps you select highly optimized models by SOYNET. Check the perfomance improvement with Soynet and other frameworks.",
+                        description: "Model Market helps you select highly optimized models by Soynet. Check the perfomance improvement with Soynet and other frameworks.",
                         thumbnail: "img/soynet/taras-shypka-iFSvn82XfGo-unsplash.jpg",
-                        buttonText: "Model Market",
-                        buttonAction : () => this.$router.push('/model-market')
+                        buttonText: "Check ModelMarket",
+                        buttonAction: () => this.$router.push('/model-market')
                     },
                     {
                         category: "Inference",
                         title: "Inference Optimizer",
-                        description: ["Reduce memory consumption and increse the speed of the inference engine.", "Simple API like integration with software application.", "CPU,GPU and TPU support capability."],
+                        description: [
+                            "Integrate your software with simple API.",
+                            "Increase the inference speed of your Models.",
+                            "Support GPU. (CPU, and TPU coming soon)",
+                            `We will add the following Features.
+                            <div class="ml-2 mt-2" style="font-size : 0.9rem">
+                                <p class="mb-0" style="font-weight : 450">Extended Support for Standards</p>
+                                <p>
+                                    There are a range of standards offered by numerous vendors to help run AI models. SoyNet supports not only NVIDIA CUDA technology but also OpenCL.
+                                </p>
+                            </div>
+                             
+                            `,
+                        ],
                         thumbnail: "img/soynet/possessed-photography-AUxh4Tccuf8-unsplash.jpg",
-                        buttonText: "Read More",
-                        buttonAction : () => this.$router.push('/blogs')
+                        buttonText: "Learn More",
+                        buttonAction: () => this.$router.push('/blogs')
                     }
                 ]
             }
